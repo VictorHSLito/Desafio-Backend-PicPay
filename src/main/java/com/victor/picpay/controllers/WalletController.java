38d654841e31ce0a90a3ec55409a659e2042a216
Victor Hugo <victorferrarihugo@hotmail.com>
2025-06-14 16:15:40 -0300

    feat: Create Repository, Service and Controller

---
+++ src/main/java/com/victor/picpay/controllers/WalletController.java
@@ -0,0 +1,25 @@
+package com.victor.picpay.controllers;
+
+import com.victor.picpay.dtos.WalletDTO;
+import com.victor.picpay.entities.Wallet;
+import com.victor.picpay.services.WalletService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/wallet")
+public class WalletController {
+
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Wallet> createWallet(@RequestBody WalletDTO walletDTO) {
+        var wallet = walletService.createWallet(walletDTO);
+        return ResponseEntity.ok(wallet);
+    }
+}
---
+++ src/main/java/com/victor/picpay/services/WalletService.java
@@ -0,0 +1,20 @@
+package com.victor.picpay.services;
+
+import com.victor.picpay.dtos.WalletDTO;
+import com.victor.picpay.entities.Wallet;
+import com.victor.picpay.repositories.WalletRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletService {
+
+    private final WalletRepository walletRepository;
+
+    public WalletService(WalletRepository walletRepository) {
+        this.walletRepository = walletRepository;
+    }
+
+    public Wallet createWallet(WalletDTO walletDTO) {
+        return walletRepository.save(walletDTO.toWallet());
+    }
+}

1f8ffefa231886f1f55a07b2bedee536734a9cb7
Victor <victorferrarihugo@hotmail.com>
2025-12-26 15:54:54 -0300

    feat: Create SecurityConfiguration class just for test

--- src/main/java/com/victor/picpay/security/SecurityConfiguration.java
+++ src/main/java/com/victor/picpay/security/SecurityConfiguration.java
@@ -1,8 +1,23 @@
 package com.victor.picpay.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-class SecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll());
 
+        return http.build();
+    }
 }

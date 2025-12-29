8be1f4db02df93024eea171f5714cabe7bae481e
Victor <victorferrarihugo@hotmail.com>
2025-12-29 14:48:18 -0300

    feat: Create LoginController and LoginService

---
+++ src/main/java/com/victor/picpay/services/LoginService.java
@@ -0,0 +1,37 @@
+package com.victor.picpay.services;
+
+import com.victor.picpay.dtos.LoginDTO;
+import com.victor.picpay.repositories.UserRepository;
+import com.victor.picpay.security.jwt.JwtTokenService;
+import com.victor.picpay.security.jwt.RecoveryJwtTokenDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtTokenService jwtTokenService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public RecoveryJwtTokenDTO verifyUserLogin(LoginDTO loginDTO) {
+        var user = userRepository.findUserByEmail(loginDTO.email());
+
+        if (user.isPresent()) {
+            String password = user.get().getPassword();
+
+            if (passwordEncoder.matches(loginDTO.password(), password)) {
+                String token = jwtTokenService.generateToken(loginDTO);
+
+                return new RecoveryJwtTokenDTO(token);
+            }
+        }
+        return null;
+    }
+}

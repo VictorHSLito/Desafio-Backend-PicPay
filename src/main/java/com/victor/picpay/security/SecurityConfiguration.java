d7fdc47597659754098d66a97bb0498a2096e164
Victor <victorferrarihugo@hotmail.com>
2025-12-30 11:55:05 -0300

    feat: Create SecurityFilterChain for /login and /user endpoints

--- src/main/java/com/victor/picpay/security/SecurityConfiguration.java
+++ src/main/java/com/victor/picpay/security/SecurityConfiguration.java
@@ -1,26 +1,37 @@
 package com.victor.picpay.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 // Reference: https://docs.spring.io/spring-security/reference/features/authentication/password-storage.html
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Autowired
+    private UserAuthenticationFilter userAuthenticationFilter;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, UserAuthenticationFilter userAuthenticationFilter) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll());
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers(HttpMethod.POST, "/login/**").permitAll()
+                )
+                .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/user/**").permitAll())
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }

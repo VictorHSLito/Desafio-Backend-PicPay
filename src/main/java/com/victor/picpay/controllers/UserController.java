90ed37fcbf75bb6f587b8a14c92712b44f6ea983
Victor <victorferrarihugo@hotmail.com>
2026-02-03 22:54:04 -0300

    feat: Create new endpoint to return user data

--- src/main/java/com/victor/picpay/controllers/UserController.java
+++ src/main/java/com/victor/picpay/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.victor.picpay.controllers;
 
-import com.victor.picpay.dtos.UserDTO;
-import com.victor.picpay.entities.User;
-import com.victor.picpay.services.UserService;
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.victor.picpay.dtos.UserDTO;
+import com.victor.picpay.dtos.UserInfoDTO;
+import com.victor.picpay.services.UserService;
 
 @RestController
 @RequestMapping("/user")
@@ -20,8 +25,14 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<UserInfoDTO> createUser(@RequestBody UserDTO userDTO) {
         var user = userService.createUser(userDTO);
         return ResponseEntity.ok(user);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserInfoDTO> showInfo(@PathVariable(name = "id") String userId) {
+        var user = userService.fetchUserInfo(UUID.fromString(userId));
+        return ResponseEntity.ok().body(user);
+    };
 }
--- src/main/java/com/victor/picpay/services/UserService.java
+++ src/main/java/com/victor/picpay/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.victor.picpay.dtos.UserDTO;
+import com.victor.picpay.dtos.UserInfoDTO;
 import com.victor.picpay.entities.User;
 import com.victor.picpay.enums.UserType;
 import com.victor.picpay.exceptions.UserNotFound;
@@ -28,7 +29,7 @@ public class UserService {
         this.userMapper = userMapper;
     }
 
-    public User createUser(UserDTO userDTO) {
+    public UserInfoDTO createUser(UserDTO userDTO) {
         var wallet = userRepository.findByCpfCnpjOrEmail(userDTO.email(), userDTO.cpfCnpj());
         if (wallet.isPresent()) {
             throw new WalletDataAlreadyExists("CPF or CNPJ already exists");
@@ -40,7 +41,9 @@ public class UserService {
 
         user.setPassword(encryptedPassword);
 
-        return userRepository.save(user);
+        userRepository.save(user);
+
+        return userMapper.toInfoDto(user);
     }
 
     public User findUser(UUID uuid) {
@@ -48,6 +51,14 @@ public class UserService {
                 .findById(uuid)
                 .orElseThrow(() ->
                         new UserNotFound("User cannot be found on database!"));
+    }
+
+    public UserInfoDTO fetchUserInfo(UUID id) {
+        User user = userRepository
+                .findById(id)
+                .orElseThrow(() ->
+                        new UserNotFound("User cannot be found on database!"));
+        return userMapper.toInfoDto(user);
     }
 
 
